98f2adb2978e537f89f9e526a27235421e74eb66
hmilly <milstowers@gmail.com>
2021-09-30 18:54:57 +0100

    added mole img, updated css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,11 @@
-import * as React from "react"
-import { Container } from "../styles/Container.styled"
-
-const headingStyles = {
-  height: 60,
-  margin: 0,
-}
-const paragraphStyles = {
-  height: 30,
-  margin: 0,
-}
-
-const areaStyles = {
-  listStyleType: "none",
-  display: "grid",
-  gridTemplateColumns: "repeat(3, 1fr)",
-  gridTemplateRows: "repeat(3, 1fr)",
-  gridGap: 0,
-  height: 450,
-  width: 450,
-  margin: "0 auto",
-  padding: 0,
-  backgroundColor: "lightBlue",
-}
-const liStyle = {
-  border: "solid 1px black",
-  padding: 0,
-}
-
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
+import * as React from "react";
+import { useState } from "react";
+import { Container } from "../styles/Container.styled";
+import mole from "../images/mole.png";
 
 const IndexPage = () => {
+  const [hole, setHole] = useState(<img src={mole} alt="mole" />);
+
   return (
     <Container>
       <header>
@@ -41,18 +14,20 @@ const IndexPage = () => {
         <p>Get him!</p>
       </header>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>{hole}</li>
+        <li>
+          <img src={mole} alt="mole" />
+        </li>
+        <li>{hole}</li>
+        <li>{hole}</li>
+        <li>{hole}</li>
+        <li>{hole}</li>
+        <li>{hole}</li>
+        <li>{hole}</li>
+        <li>{hole}</li>
       </ul>
     </Container>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
--- src/styles/Container.styled.js
+++ src/styles/Container.styled.js
@@ -1,59 +1,63 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.main`
-    font-family: "-apple-system, Roboto, sans-serif, serif";
-    width: 90vw;
-    height: 90vh;
-    margin: 0 auto;
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr 4fr;
-    gap: 0% 0%;
-    grid-auto-flow: row;
-    justify-content: center;
-    align-content: center;
-    justify-items: center;
-    align-items: center;
-    grid-template-areas: 
+  font-family: "-apple-system, Roboto, sans-serif, serif";
+  width: 90vw;
+  height: 90vh;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 4fr;
+  gap: 0% 0%;
+  grid-auto-flow: row;
+  justify-content: center;
+  align-content: center;
+  justify-items: center;
+  align-items: center;
+  grid-template-areas:
     "header"
-    "ul"; 
- 
-    header {
-        grid-area: header;
-        width: 500px;
-        height: 80px;
-        text-align: center;
+    "ul";
+
+  header {
+    grid-area: header;
+    width: 500px;
+    height: 80px;
+    text-align: center;
+    h1,
+    p {
+      padding: 0;
+      margin: 0;
     }
-    h1, p{
-        padding: 0;
-        margin: 0;
+  }
+
+  ul {
+    grid-area: ul;
+    display: grid;
+    grid-template: repeat(3, 1fr);
+    gap: 2px 2px;
+    grid-auto-flow: row;
+    list-style-type: none;
+    width: 390px;
+    height: 390px;
+    grid-template-areas:
+      ". . ."
+      ". . ."
+      ". . .";
+    background-color: green;
+    padding: 0;
+    li {
+      width: 80%;
+      height: auto;
+      border: solid 1px black;
+      padding: 0;
+      margin: 0;
+      background-color: brown;
+      border-radius: 80% 20% 80% 20%;
+      img {
+        width: 100px;
+        height: auto;
+        padding: 15px;
+      }
     }
-
-
-    ul {
-        grid-area: ul;
-        display: grid;
-        grid-template: repeat(3, 1fr);
-        gap: 0px 0px;
-        grid-auto-flow: row;
-        list-style-type: none;
-        width: 400px;
-        height: 400px;
-        grid-template-areas: 
-        ". . ."
-        ". . ."
-        ". . ."; 
-        background-color: rgba(124, 232, 232, 0.67);
-        padding: 0;
-    }
-
-    li{
-        border: solid 1px black;
-        padding: 0;
-        margin: 0;
-    }
-
-    `
-
-
-
+  }
+`;
